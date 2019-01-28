d3a29b224ba88e850348547362b030a663378b92
Sergio <fullsanmons@gmail.com>
2019-01-27 20:53:59 -0500

    js file at lesson 10

--- src/js/home.js
+++ src/js/home.js
@@ -11,7 +11,7 @@ const getUserAll = new Promise(function(todoBien, todoMal) {
   // llamar a un api
   setTimeout(function() {
     // luego de 3 segundos
-    todoBien('se acabó el tiempo');
+    todoBien('se acabá el tiempo');
   }, 5000)
 })
 
@@ -19,13 +19,13 @@ const getUser = new Promise(function(todoBien, todoMal) {
   // llamar a un api
   setTimeout(function() {
     // luego de 3 segundos
-    todoBien('se acabó el tiempo 3');
+    todoBien('se acabá el tiempo 3');
   }, 3000)
 })
 
 // getUser
 //   .then(function() {
-//     console.log('todo está bien en la vida')
+//     console.log('todo estÃ¡ bien en la vida')
 //   })
 //   .catch(function(message) {
 //     console.log(message)
@@ -42,7 +42,7 @@ Promise.race([
   console.log(message)
 })
 
-
+// Ajax con JQuery 
 
 $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
   method: 'GET',
@@ -54,7 +54,9 @@ $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
   }
 })
 
-fetch('https://randomuser.me/api/dsfdsfsd')
+// Ajax con JavaScript
+
+fetch('https://randomuser.me/api')
   .then(function (response) {
     // console.log(response)
     return response.json()
@@ -63,7 +65,7 @@ fetch('https://randomuser.me/api/dsfdsfsd')
     console.log('user', user.results[0].name.first)
   })
   .catch(function() {
-    console.log('algo falló')
+    console.log('algo falla')
   });
 
 
@@ -74,140 +76,48 @@ fetch('https://randomuser.me/api/dsfdsfsd')
   // animation
   async function getData(url) {
     const response = await fetch(url);
-    const data = await response.json();
-    if (data.data.movie_count > 0) {
-      // aquí se acaba
-      return data;
-    }
-    // si no hay pelis aquí continua
-    throw new Error('No se encontró ningun resultado');
-  }
-  const $form = document.getElementById('form');
-  const $home = document.getElementById('home');
-  const $featuringContainer = document.getElementById('featuring');
-
-
-  function setAttributes($element, attributes) {
-    for (const attribute in attributes) {
-      $element.setAttribute(attribute, attributes[attribute]);
-    }
-  }
-  const BASE_API = 'https://yts.am/api/v2/';
-
-  function featuringTemplate(peli) {
-    return (
-      `
-      <div class="featuring">
-        <div class="featuring-image">
-          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
-        </div>
-        <div class="featuring-content">
-          <p class="featuring-title">Pelicula encontrada</p>
-          <p class="featuring-album">${peli.title}</p>
-        </div>
-      </div>
-      `
-    )
+    const data = await response.json()
+    return data;
   }
 
-  $form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    $home.classList.add('search-active')
-    const $loader = document.createElement('img');
-    setAttributes($loader, {
-      src: 'src/images/loader.gif',
-      height: 50,
-      width: 50,
-    })
-    $featuringContainer.append($loader);
+  const actionList = await getData('https://yts.am/api/v2/list_movies.json?genre=action')
+  const dramaList = await getData('https://yts.am/api/v2/list_movies.json?genre=drama')
+  const animationList = await getData('https://yts.am/api/v2/list_movies.json?genre=animation')
+  console.log(actionList, dramaList, animationList)
 
-    const data = new FormData($form);
-    try {
-      const {
-        data: {
-          movies: pelis
-        }
-      } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
-
-      const HTMLString = featuringTemplate(pelis[0]);
-      $featuringContainer.innerHTML = HTMLString;
-    } catch(error) {
-      alert(error.message);
-      $loader.remove();
-      $home.classList.remove('search-active');
-    }
-  })
-
-  function videoItemTemplate(movie, category) {
-    return (
-      `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category=${category}>
-        <div class="primaryPlaylistItem-image">
-          <img src="${movie.medium_cover_image}">
+  function videoItemTemplate(movie){
+    return(
+      `<div class="primaryPlayListItem">
+        <div class="primaryPlayListItem-image">
+          <img src="${movie.medium_cover_image}" />
         </div>
         <h4 class="primaryPlaylistItem-title">
           ${movie.title}
         </h4>
       </div>`
-    )
+    ) 
   }
-  function createTemplate(HTMLString) {
+
+  const $actionContainer = document.querySelector('#action');
+  const $dramaContainer = document.getElementById('#drama');
+  const $animationContainer = document.getElementById('#animation');
+
+    
+  actionList.data.movies.forEach((movie)=>{
+    debugger
+    const HTMLString = videoItemTemplate(movie);
     const html = document.implementation.createHTMLDocument();
     html.body.innerHTML = HTMLString;
-    return html.body.children[0];
-  }
-  function addEventClick($element) {
-    $element.addEventListener('click', () => {
-      // alert('click')
-      showModal($element)
-    })
-  }
-  function renderMovieList(list, $container, category) {
-    // actionList.data.movies
-    $container.children[0].remove();
-    list.forEach((movie) => {
-      const HTMLString = videoItemTemplate(movie, category);
-      const movieElement = createTemplate(HTMLString);
-      $container.append(movieElement);
-      const image = movieElement.querySelector('img');
-      image.addEventListener('load', (event) => {
-        event.srcElement.classList.add('fadeIn');
-      })
-      addEventClick(movieElement);
-    })
-  }
-
-  async function cacheExist(category) {
-    const listName = `${category}List`;
-    const cacheList = window.localStorage.getItem(listName);
-
-    if (cacheList) {
-      return JSON.parse(cacheList);
-    }
-    const { data: { movies: data } } = await getData(`${BASE_API}list_movies.json?genre=${category}`)
-    window.localStorage.setItem(listName, JSON.stringify(data))
-
-    return data;
-  }
-
-  // const { data: { movies: actionList} } = await getData(`${BASE_API}list_movies.json?genre=action`)
-  const actionList = await cacheExist('action');
-  // window.localStorage.setItem('actionList', JSON.stringify(actionList))
-  const $actionContainer = document.querySelector('#action');
-  renderMovieList(actionList, $actionContainer, 'action');
-
-  const dramaList = await await cacheExist('drama');
-  const $dramaContainer = document.getElementById('drama');
-  renderMovieList(dramaList, $dramaContainer, 'drama');
-
-  const animationList = await await cacheExist('animation');
-  const $animationContainer = document.getElementById('animation');
-  renderMovieList(animationList, $animationContainer, 'animation');
+    $actionContainer.append(html.body.children[0]);
+    // $actionContainer.innerHTML += HTMLString;
+  });
 
 
 
 
-
-
+  const $featuringContainer = document.getElementById('#featuring');
+  const $form = document.getElementById('#form');
+  const $home = document.getElementById('#home');
 
 
   // const $home = $('.home .list #item');
@@ -217,46 +127,5 @@ fetch('https://randomuser.me/api/dsfdsfsd')
 
   const $modalTitle = $modal.querySelector('h1');
   const $modalImage = $modal.querySelector('img');
-  const $modalDescription = $modal.querySelector('p');
-
-  function findById(list, id) {
-    return list.find(movie => movie.id === parseInt(id, 10))
-  }
-
-  function findMovie(id, category) {
-    switch (category) {
-      case 'action' : {
-        return findById(actionList, id)
-      }
-      case 'drama' : {
-        return findById(dramaList, id)
-      }
-      default: {
-        return findById(animationList, id)
-      }
-    }
-  }
-
-  function showModal($element) {
-    $overlay.classList.add('active');
-    $modal.style.animation = 'modalIn .8s forwards';
-    const id = $element.dataset.id;
-    const category = $element.dataset.category;
-    const data = findMovie(id, category);
-
-    $modalTitle.textContent = data.title;
-    $modalImage.setAttribute('src', data.medium_cover_image);
-    $modalDescription.textContent = data.description_full
-  }
-
-  $hideModal.addEventListener('click', hideModal);
-  function hideModal() {
-    $overlay.classList.remove('active');
-    $modal.style.animation = 'modalOut .8s forwards';
-
-  }
-
-
-
-
+  const $modalDescription = $modal.querySelector('p'); 
 })()
