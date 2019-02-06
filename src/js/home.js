f3f55d30900477a5a919fb0398fa936bffb45502
Sergio Nino <sergioanino@qdata.io>
2019-02-06 12:01:26 -0500

    Retos y guardar listas en localStorage

--- src/js/home.js
+++ src/js/home.js
@@ -1,27 +1,27 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+// console.log('hola mundo!');
+// const noCambia = "Leonidas";
 
-let cambia = "@LeonidasEsteban"
+// let cambia = "@LeonidasEsteban"
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+// function cambiarNombre(nuevoNombre) {
+//   cambia = nuevoNombre
+// }
 
-const getUserAll = new Promise(function(todoBien, todoMal) {
-  // llamar a un api
-  setTimeout(function() {
-    // luego de 3 segundos
-    todoBien('se acabá el tiempo');
-  }, 5000)
-})
+// const getUserAll = new Promise(function(todoBien, todoMal) {
+//   // llamar a un api
+//   setTimeout(function() {
+//     // luego de 3 segundos
+//     todoBien('se acabá el tiempo');
+//   }, 5000)
+// })
 
-const getUser = new Promise(function(todoBien, todoMal) {
-  // llamar a un api
-  setTimeout(function() {
-    // luego de 3 segundos
-    todoBien('se acabá el tiempo 3');
-  }, 3000)
-})
+// const getUser = new Promise(function(todoBien, todoMal) {
+//   // llamar a un api
+//   setTimeout(function() {
+//     // luego de 3 segundos
+//     todoBien('se acabá el tiempo 3');
+//   }, 3000)
+// })
 
 // getUser
 //   .then(function() {
@@ -31,49 +31,115 @@ const getUser = new Promise(function(todoBien, todoMal) {
 //     console.log(message)
 //   })
 
-Promise.race([
-  getUser,
-  getUserAll,
-])
-.then(function(message) {
-  console.log(message);
-})
-.catch(function(message) {
-  console.log(message)
-})
+// Promise.race([
+//   getUser,
+//   getUserAll,
+// ])
+// .then(function(message) {
+//   console.log(message);
+// })
+// .catch(function(message) {
+//   console.log(message)
+// })
 
 // Ajax con JQuery 
 
-$.ajax('https://randomuser.me/api/sdfdsfdsfs', {
-  method: 'GET',
-  success: function(data) {
-    console.log(data)
-  },
-  error: function(error) {
-    console.log(error)
-  }
-})
+// $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
+//   method: 'GET',
+//   success: function(data) {
+//     console.log(data)
+//   },
+//   error: function(error) {
+//     console.log(error)
+//   }
+// })
 
 // Ajax con JavaScript
 
-fetch('https://randomuser.me/api')
-  .then(function (response) {
-    // console.log(response)
-    return response.json()
-  })
-  .then(function (user) {
-    console.log('user', user.results[0].name.first)
-  })
-  .catch(function() {
-    console.log('algo falla')
-  });
+// fetch('https://randomuser.me/api')
+//   .then(function (response) {
+//     // console.log(response)
+//     return response.json()
+//   })
+//   .then(function (user) {
+//     console.log('user', user.results[0].name.first)
+//   })
+//   .catch(function() {
+//     console.log('algo falla')
+//   });
+
+
 
 
 (async function load() {
-  // await
-  // action
-  // terror
-  // animation
+  // Primera parte del reto: Usuarios: 
+  async function getUser(url) {
+    const response = await fetch(url);
+    const dataUser = await response.json();
+    return dataUser.results[0];
+  }
+
+  const USER_URL = 'https://randomuser.me/api';
+  const NUMBER_USERS = 8;
+
+  function userTemplate(user){
+    return(`
+      <li class="playlistFriends-item">
+        <a href="#">
+          <img src="${user.picture.large}" alt="echame la culpa" />
+          <span>${user.name.first} ${user.name.last}</span>
+        </a>
+      </li >
+    `);
+  }
+
+  async function usersList (numberUsers){
+    const usersArray = new Array();
+    for (let index = 0; index < numberUsers; index++) {
+      await getUser(USER_URL)
+      .then((user) => {
+        usersArray.push(user);
+      });
+    }
+    return usersArray;
+  } 
+
+  function createUserTemplate(userHTML) {
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = userHTML;
+    return html.body.children[0];
+  }
+   
+  function renderPlaylist(list, $container) {
+    $container.children[0].remove();
+    list.forEach((user) => {
+      const userHTML = userTemplate(user);
+      const userElement = createUserTemplate(userHTML);
+      $container.append(userElement);
+      const image = userElement.querySelector('img');
+      image.addEventListener('load', (event) => {
+        event.srcElement.classList.add('fadeIn');
+      });
+    });
+  };
+  
+  const userList = await usersList(NUMBER_USERS);
+  const $playlistFriends = document.querySelector('.playlistFriends');
+  renderPlaylist(userList, $playlistFriends);
+  // Fin reto primera parte
+
+  // Reto segunda parte: top 10 peliculas
+
+  function topMoviesTemplate(peli, category){
+    return(`
+      <li class="myPlaylist-item" data-id="${peli.id}" data-category="${category}">
+        <a href="#">
+          <span>${peli.title}</span>
+        </a>
+      </li>
+    `);
+  }
+  
   async function getData(url) {
     const response = await fetch(url);
     const data = await response.json();
@@ -135,8 +201,6 @@ fetch('https://randomuser.me/api')
     
   });
   
-  
-  
 
   function videoItemTemplate(movie, category){
     return(
@@ -156,6 +220,54 @@ fetch('https://randomuser.me/api')
     html.body.innerHTML = HTMLString;
     return html.body.children[0];
   }
+
+  const $modal = document.getElementById('modal');
+  const $overlay = document.getElementById('overlay');
+  const $hideModal = document.getElementById('hide-modal');
+
+  const $modalTitle = $modal.querySelector('h1');
+  const $modalImage = $modal.querySelector('img');
+  const $modalDescription = $modal.querySelector('p'); 
+
+  function findById(list, id) {
+
+    return list.find(movie => movie.id === parseInt(id, 10));
+  }
+
+  function findMovie(id, category) {
+    switch (category) {
+      case 'action': {
+        return findById(actionList, id);
+      }
+      case 'drama': {
+        return findById(dramaList, id);
+      }
+      case 'animation': {
+        return findById(animationList, id);
+      }
+      default: {
+        return findById(topTenList, id);
+      }
+    }
+  }
+
+  function showModal($element) {
+    $overlay.classList.add('active');
+    $modal.style.animation = 'modalIn .8s forwards';
+    const id = $element.dataset.id;
+    const category = $element.dataset.category;
+    const data = findMovie(id, category);
+    console.log($element.category);
+    $modalTitle.textContent = data.title;
+    $modalImage.setAttribute('src', data.medium_cover_image);
+    $modalDescription.textContent = data.description_full;
+  }
+  
+  function hideModal() {
+    $overlay.classList.remove('active');
+    $modal.style.animation = 'modalOut .8s forwards';
+  }
+  $hideModal.addEventListener('click', hideModal);
 
   function addEventClick($element){
     $element.addEventListener('click', () => {
@@ -177,72 +289,42 @@ fetch('https://randomuser.me/api')
       addEventClick(movieElement);
     });
   };
+  function renderTopList(list, $container, category){
+    $container.children[0].remove();
+    list.forEach((movie)=>{
+      const HTMLString = topMoviesTemplate(movie, category);
+      const movieElement = createTemplate(HTMLString);
+      $container.append(movieElement);
+      addEventClick(movieElement);
+    });
+  };
+
+  const {data: {movies: topTenList}} = await getData(`${BASE_API}list_movies.json?limit=10&sort_by=like_count`)
+  $myTopTenContainer = document.querySelector('.myPlaylist');
+  renderTopList(topTenList, $myTopTenContainer, 'top10');
+
   const {data: {movies: actionList}} = await getData(`${BASE_API}list_movies.json?genre=action`)
+  localStorage.setItem('actionList', JSON.stringify(actionList));
   const $actionContainer = document.querySelector('#action');
   renderMovieList(actionList, $actionContainer, 'action');
-
+  
   const {data: {movies: dramaList}} = await getData(`${BASE_API}list_movies.json?genre=drama`)
+  localStorage.setItem('dramaList', JSON.stringify(dramaList));
   const $dramaContainer = document.getElementById('drama');
   renderMovieList(dramaList, $dramaContainer, 'drama');
-
+  
   const {data: {movies: animationList}} = await getData(`${BASE_API}list_movies.json?genre=animation`)
+  localStorage.setItem('animationList', JSON.stringify(animationList));
   const $animationContainer = document.getElementById('animation');
   renderMovieList(animationList, $animationContainer, 'animation');
-  
-
-
 
   
-
   // const $home = $('.home .list #item');
-  const $modal = document.getElementById('modal');
-  const $overlay = document.getElementById('overlay');
-  const $hideModal = document.getElementById('hide-modal');
 
-  const $modalTitle = $modal.querySelector('h1');
-  const $modalImage = $modal.querySelector('img');
-  const $modalDescription = $modal.querySelector('p'); 
 
-<<<<<<< HEAD
+
+
   
-=======
-  function findById(list, id){
-    return list.find(movie => movie.id === parseInt(id, 10));
-  }
 
-  function findMovie(id, category){
-    switch (category){
-      case 'action': {
-        return findById(actionList, id);
-      }
-      case 'drama': {
-        return findById(dramaList, id);
-      } 
-      default: {
-        return findById(animationList, id);
-      }
-    }
-  }
 
->>>>>>> 892cb5b4bc35ee8539d5bee7661b1c68e75bd602
-  function showModal($element){
-    $overlay.classList.add('active');
-    $modal.style.animation = 'modalIn .8s forwards';
-    const id = $element.dataset.id;
-    const category = $element.dataset.category;
-<<<<<<< HEAD
-    const data = findMovie(id, category)
-=======
-    const data = findMovie(id, category);
-    $modalTitle.textContent  = data.title;
-    $modalImage.setAttribute('src', data.medium_cover_image);
-    $modalDescription.textContent = data.description_full;
->>>>>>> 892cb5b4bc35ee8539d5bee7661b1c68e75bd602
-  }
-  $hideModal.addEventListener('click', hideModal);
-  
-  function hideModal() {
-    $overlay.classList.remove('active');
-    $modal.style.animation = 'modalOut .8s forwards';
-  }
 })()
